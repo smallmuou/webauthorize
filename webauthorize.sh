b2d9968f045f71955cee7afcec71134789766457
wenva <lvyexuwenfa100@star-net.cn>
2014-10-22 16:47:58 +0800

    remove unuse code

--- webauthorize.sh
+++ webauthorize.sh
@@ -115,13 +115,6 @@ do
     esac
 done
 
-        if [ "$IP" == "" ]; then
-            IP=`ifconfig $OPTARG|awk '/inet /{print $2}'`
-            if [ "$IP" == "" ]; then
-                error "The interface $OPTARG doesn't exist."
-                exit -1
-            fi
-        fi
 # if not assign ip, get default ip
 if [ "$IP" == "" ]; then
 
